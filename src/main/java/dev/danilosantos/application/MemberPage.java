4265ee06d0fa4d724257199b7d93d646adfcc497
Danilo Santos <danilo.santos0404@hotmail.com>
2023-08-15 17:12:38 -0300

    Limpeza de código na classe MemberPage

--- src/main/java/dev/danilosantos/application/MemberPage.java
+++ src/main/java/dev/danilosantos/application/MemberPage.java
@@ -1,27 +1,19 @@
 package dev.danilosantos.application;
 
 import dev.danilosantos.application.util.ClearScreen;
-import dev.danilosantos.domain.util.GenerateMemberCardNumber;
 import dev.danilosantos.application.util.Strings;
 import dev.danilosantos.domain.MemberResource;
 import dev.danilosantos.infrasctructure.Document;
-import dev.danilosantos.infrasctructure.Member;
 import dev.danilosantos.infrasctructure.enums.DocumentType;
 import dev.danilosantos.infrasctructure.file_management.CreateFoldersAndFiles;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class MemberPage {
-
-    GenerateMemberCardNumber generateCardNumber = new GenerateMemberCardNumber();
     MemberResource memberResource = new MemberResource();
     CreateFoldersAndFiles createFoldersAndFiles = new CreateFoldersAndFiles();
-
     String name, cardNumber;
     Document document;
-    Member member, updatedMember;
 
     public MemberPage() {
         if (!createFoldersAndFiles.create()) {
@@ -35,38 +27,20 @@ public class MemberPage {
             System.out.println(Strings.MENU_MEMBER_PAGE);
 
             switch (scanner.nextInt()) {
-                case 1:
-                    insertMember(scanner);
-                    break;
-
-                case 2:
-                    findByDocument(scanner);
-                    break;
-
-                case 3:
-                    findByName(scanner);
-                    break;
-
-                case 4:
-                    updateMember(scanner);
-                    break;
-
-                case 5:
-                    deleteMember(scanner);
-                    break;
-                case 6:
-                    ClearScreen.clear();
-                    findAll();
-                    break;
-                case 7:
+                case 1 -> insertMember(scanner);
+                case 2 -> findByDocument(scanner);
+                case 3 -> findByName(scanner);
+                case 4 -> updateMember(scanner);
+                case 5 -> deleteMember(scanner);
+                case 6 -> findAll();
+                case 7 -> {
                     ClearScreen.clear();
                     running = false;
-                    break;
-
-                default:
+                }
+                default -> {
                     ClearScreen.clear();
                     System.out.println(Strings.ERROR_INVALID_VALUE);
-                    break;
+                }
             }
         }
     }
@@ -124,36 +98,31 @@ public class MemberPage {
     }
 
     private String getNameFromScanner(Scanner scanner) {
-        boolean isSpelledRight = false;
-        while (isSpelledRight == false) {
+        while (true) {
             System.out.print(Strings.WRITE_MEMBER_NAME);
-            String localName = scanner.nextLine();
-            if (localName.matches("[\\p{L}\\s]+") && localName.trim() != "") {
-                isSpelledRight = true;
-                return localName.trim();
+            String localName = scanner.nextLine().trim();
+            if (localName.matches("[\\p{L}\\s]+") && !localName.isEmpty()) {
+                return localName;
             } else {
                 ClearScreen.clear();
                 System.out.println(Strings.ERROR_INVALID_NAME);
             }
         }
-        return null;
     }
 
     private Document verificationToInsertMemberOnDocument(Document document, Scanner scanner) {
-        Document documentToInsert = document;
-        if (memberResource.verifyIfDocumentAlredyExists(documentToInsert) == false) {
-            return documentToInsert;
+        if (!memberResource.verifyIfDocumentAlredyExists(document)) {
+            return document;
         } else {
             System.out.println(Strings.ERROR_DOCUMENT_ALREDY_EXISTS);
             System.out.println(Strings.MENU_DOCUMENT_ALREDY_EXISTS);
             switch (scanner.nextInt()) {
-                case 1:
+                case 1 -> {
                     return getDocumentFromScanner(scanner);
-                case 2:
-                    break;
-                default:
-                    System.out.println(Strings.ERROR_INVALID_VALUE);
-                    break;
+                }
+                case 2 -> {
+                }
+                default -> System.out.println(Strings.ERROR_INVALID_VALUE);
             }
         }
         return null;
@@ -163,7 +132,7 @@ public class MemberPage {
         DocumentType documentType = null;
         String documentValue = null;
         boolean isSpelledRight = false;
-        while (isSpelledRight == false) {
+        while (!isSpelledRight) {
             System.out.print(Strings.WRITE_DOCUMENT_TYPE);
             String typeFromScanner = scanner.next().toUpperCase();
             if (typeFromScanner.equals("RG") || typeFromScanner.equals("CPF")) {
@@ -174,7 +143,7 @@ public class MemberPage {
             }
         }
         isSpelledRight = false;
-        while (isSpelledRight == false) {
+        while (!isSpelledRight) {
             System.out.print(Strings.writeDocumentValue(documentType.toString()));
             documentValue = scanner.next();
             if (documentValue.matches("[+-]?\\d*(\\.\\d+)?")) {
@@ -184,10 +153,6 @@ public class MemberPage {
             }
         }
 
-        if (documentType != null || documentValue != null) {
-            return new Document(documentType, documentValue);
-        } else {
-            return null;
-        }
+        return new Document(documentType, documentValue);
     }
 }
